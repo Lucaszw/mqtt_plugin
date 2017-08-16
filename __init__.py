7ab54d662d9c93862268ae6bfcd8bced64ca5204
lucaszw <lucas.zeer@gmail.com>
2017-08-16 18:54:36 -0400

    add methods for handling loading protocols

--- __init__.py
+++ __init__.py
@@ -5,6 +5,9 @@ from microdrop.app_context import get_app, get_hub_uri
 from microdrop.plugin_helpers import get_plugin_info
 from microdrop.plugin_manager import (PluginGlobals, Plugin, IPlugin,
                                       implements, emit_signal)
+from microdrop.protocol import protocol_from_dict
+
+from zmq_plugin.schema import pandas_object_hook, PandasJsonEncoder
 import paho_mqtt_helpers as pmh
 import path_helpers as ph
 
@@ -29,14 +32,18 @@ class MqttPlugin(pmh.BaseMqttReactor, Plugin):
         super(MqttPlugin, self).__init__()
         self.name = self.plugin_name
         self.start()
-        self.mqtt_client.subscribe("microdrop/dmf-device-ui/change-step")
-        self.mqtt_client.subscribe("microdrop/dmf-device-ui/delete-step")
-        self.mqtt_client.subscribe("microdrop/dmf-device-ui/insert-step")
-        self.mqtt_client.subscribe("microdrop/dmf-device-ui/change-protocol-state")
 
     ###########################################################################
     # MicroDrop pyutilib plugin handlers
     # ==================================
+    def on_connect(self, client, userdata, flags, rc):
+        self.mqtt_client.subscribe("microdrop/dmf-device-ui/change-step")
+        self.mqtt_client.subscribe("microdrop/dmf-device-ui/delete-step")
+        self.mqtt_client.subscribe("microdrop/dmf-device-ui/insert-step")
+        self.mqtt_client.subscribe("microdrop/dmf-device-ui/change-protocol-state")
+        self.mqtt_client.subscribe("microdrop/dmf-device-ui/change-repeat")
+        self.mqtt_client.subscribe("microdrop/data-controller/load-protocol")
+
     def on_message(self, client, userdata, msg):
         '''
         Callback for when a ``PUBLISH`` message is received from the broker.
@@ -49,6 +56,10 @@ class MqttPlugin(pmh.BaseMqttReactor, Plugin):
             self.insert_step(json.loads(msg.payload))
         if msg.topic == "microdrop/dmf-device-ui/change-protocol-state":
             self.change_protocol_state(json.loads(msg.payload))
+        if msg.topic == "microdrop/dmf-device-ui/change-repeat":
+            self.change_protocol_repeat(json.loads(msg.payload))
+        if msg.topic == "microdrop/data-controller/load-protocol":
+            self.load_protocol(json.loads(msg.payload, object_hook=pandas_object_hook))
 
     def on_plugin_disable(self):
         """
@@ -108,5 +119,44 @@ class MqttPlugin(pmh.BaseMqttReactor, Plugin):
             emit_signal("on_run_protocol", [None, None])
         else:
             emit_signal("on_run_protocol", [None, None])
+
+    def change_protocol_repeat(self, val):
+        # XXX: Manually updating gtk text entry:
+        app = get_app()
+        text_entry = app.protocol_controller.textentry_protocol_repeats
+        text_entry.set_text(str(val))
+        emit_signal("on_protocol_repeats_changed")
+
+    def load_protocol(self, protocol_dict):
+        app = get_app()
+        protocol = protocol_from_dict(protocol_dict)
+        app.protocol_controller.modified = True
+        emit_signal("on_protocol_changed")
+        app.protocol_controller.activate_protocol(protocol)
+
+    def on_protocol_repeats_changed(self):
+        # TODO: Make this event triggered by microdrop (or implement)
+        #      altertnative in some form of protocol controller plugin
+        app = get_app()
+        text_entry = app.protocol_controller.textentry_protocol_repeats
+        val = text_entry.get_text()
+        self.mqtt_client.publish("microdrop/mqtt-plugin/protocol-repeats-changed",val)
+
+    def on_protocol_changed(self):
+        app = get_app()
+
+        # TODO hook in protocol name with webui (should be indexed by default)
+        if app.protocol.name is None:
+            app.protocol.name = "unnamed"
+
+        self.mqtt_client.publish("microdrop/mqtt-plugin/protocol-changed",
+                                 app.protocol.to_json(), retain=True)
+
+    def on_protocol_swapped(self, old_protocol, protocol):
+        if protocol.name is None:
+            protocol.name = "unnamed"
+
+        self.mqtt_client.publish("microdrop/mqtt-plugin/protocol-swapped",
+                                 protocol.to_json(), retain=True)
 
 PluginGlobals.pop_env()
